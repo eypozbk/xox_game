49dbce969450a51b6065c7fbf4308ec7f9a5123f
[eyp ozbk] <[eyupozbek50@gmail.com]>
2022-09-01 00:31:58 +0300

    iki

--- app.js
+++ app.js
@@ -47,18 +47,43 @@ function checktie(){
 startgame();
 
 function checkRows(){
-    let row1 = blocks[0].textContent == blocks[1].textContent &&
-        blocks[0].textContent == blocks[2].textContent && blocks[0].textContent !== ""
-    let row2 = blocks[0].textContent == blocks[1].textContent &&
-        blocks[0].textContent == blocks[2].textContent && blocks[0].textContent !== ""
-    let row3 = blocks[0].textContent == blocks[1].textContent &&
-        blocks[0].textContent == blocks[2].textContent && blocks[0].textContent !== ""
+    let row1 = boxDOM[0].textContent == boxDOM[1].textContent &&
+        boxDOM[0].textContent == boxDOM[2].textContent && boxDOM[0].textContent !== ""
+
+    let row2 = boxDOM[3].textContent == boxDOM[4].textContent &&
+        boxDOM[3].textContent == boxDOM[5].textContent && boxDOM[3].textContent !== ""
+
+    let row3 = boxDOM[6].textContent == boxDOM[7].textContent &&
+        boxDOM[6].textContent == boxDOM[8].textContent && boxDOM[6].textContent !== ""
+
+        if (row1 || row2 || row3){
+            gameOver = true
+        }
+        if (row1 ) return winner = boxDOM[0].textContent
+        if (row2 ) return winner = boxDOM[3].textContent
+        if (row3 ) return winner = boxDOM[6].textContent
+
+
     }
 
+
 function checkColumns(){
+    let col1 = boxDOM[0].textContent == boxDOM[3].textContent &&
+        boxDOM[0].textContent == boxDOM[6].textContent && boxDOM[0].textContent !== ""
+
+    let col2 = boxDOM[1].textContent == boxDOM[4].textContent &&
+        boxDOM[1].textContent == boxDOM[7].textContent && boxDOM[1].textContent !== ""
+
+    let col3 = boxDOM[2].textContent == boxDOM[5].textContent &&
+        boxDOM[2].textContent == boxDOM[8].textContent && boxDOM[2].textContent !== ""
 
 }
 function checkDio(){
+    let dio1 = boxDOM[0].textContent == boxDOM[4].textContent &&
+        boxDOM[0].textContent == boxDOM[8].textContent && boxDOM[0].textContent !== ""
+
+    let dio2 = boxDOM[2].textContent == boxDOM[4].textContent &&
+        boxDOM[2].textContent == boxDOM[6].textContent && boxDOM[2].textContent !== ""
 
 }
 
