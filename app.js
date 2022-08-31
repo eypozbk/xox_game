ad2f55732e9ca6eeb4739dec90653da759495dd5
[eyp ozbk] <[eyupozbek50@gmail.com]>
2022-09-01 00:53:53 +0300

    a

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const boxDOM = document.querySelectorAll(".box")
 const playerText = document.getElementById("result")
 let turn ;
 let Player = "X"
+let gameOver = false;
+let winner 
 
  
 function startgame(){
@@ -19,6 +21,10 @@ function chooseArea(box){
     
     checkwin()
     checktie()
+    if (gameOver){
+        playerText.textContent = ` game is over , ${winner}'won `
+        boxDOM.forEach(boxDOM => boxDOM.style.pointerEvents = "none")
+    }
 }
 
 function turnPlayer (){
@@ -41,8 +47,13 @@ function checkwin(){
 
 }
 function checktie(){
+    const values = [];
+    boxDOM.forEach(box => values.push(box.textContent))
+    if (!values.includes("")) {
+        playerText.textContent = "Tie !";
+        boxDOM.forEach(box => box.style.pointerEvents = 'none');
 
-}
+}    }
 
 startgame();
 
@@ -76,7 +87,12 @@ function checkColumns(){
 
     let col3 = boxDOM[2].textContent == boxDOM[5].textContent &&
         boxDOM[2].textContent == boxDOM[8].textContent && boxDOM[2].textContent !== ""
-
+        if (col1 || col2 || col3){
+            gameOver = true
+        }
+        if (col1 ) return winner = boxDOM[0].textContent
+        if (col2 ) return winner = boxDOM[1].textContent
+        if (col3 ) return winner = boxDOM[2].textContent
 }
 function checkDio(){
     let dio1 = boxDOM[0].textContent == boxDOM[4].textContent &&
@@ -84,7 +100,11 @@ function checkDio(){
 
     let dio2 = boxDOM[2].textContent == boxDOM[4].textContent &&
         boxDOM[2].textContent == boxDOM[6].textContent && boxDOM[2].textContent !== ""
-
+        if (dio1 || dio2){
+            gameOver = true
+        }
+        if (dio1 ) return winner = boxDOM[0].textContent
+        if (dio2 ) return winner = boxDOM[2].textContent
 }
 
 
